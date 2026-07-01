bf0cffbbe8ba5b3b5bc29213bd05cb6122a23c05
ssahuji910@gmail.com <ssahuji910@gmail.com>
2026-07-02 00:22:32 +0530

    first commit

---
+++ src/main/java/com/iam/iam_server/IamServerApplication.java
@@ -0,0 +1,13 @@
+package com.iam.iam_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iam/iam_server/IamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.iam_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
